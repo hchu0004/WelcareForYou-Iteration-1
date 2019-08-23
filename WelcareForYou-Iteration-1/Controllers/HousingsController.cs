e0663e9fd511a674c9bd60e658e438f035827cd8
HSINHAN CHUNG <hchu0004@student.monash.edu>
2019-08-23 18:44:35 +1000

    Questionare controller

---
+++ WelcareForYou-Iteration-1/Controllers/HousingsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WelcareForYou_Iteration_1.Models;
+
+namespace WelcareForYou_Iteration_1.Controllers
+{
+    public class HousingsController : Controller
+    {
+        private Iteration1ModelContainer db = new Iteration1ModelContainer();
+
+        // GET: Housings
+        public ActionResult Index()
+        {
+            return View(db.Housings.ToList());
+        }
+
+        // GET: Housings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Housing housing = db.Housings.Find(id);
+            if (housing == null)
+            {
+                return HttpNotFound();
+            }
+            return View(housing);
+        }
+
+        // GET: Housings/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Housings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Suburb,NumOfRoom,MediumPrice,HouseType")] Housing housing)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Housings.Add(housing);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(housing);
+        }
+
+        // GET: Housings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Housing housing = db.Housings.Find(id);
+            if (housing == null)
+            {
+                return HttpNotFound();
+            }
+            return View(housing);
+        }
+
+        // POST: Housings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Suburb,NumOfRoom,MediumPrice,HouseType")] Housing housing)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(housing).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(housing);
+        }
+
+        // GET: Housings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Housing housing = db.Housings.Find(id);
+            if (housing == null)
+            {
+                return HttpNotFound();
+            }
+            return View(housing);
+        }
+
+        // POST: Housings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Housing housing = db.Housings.Find(id);
+            db.Housings.Remove(housing);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WelcareForYou-Iteration-1/Controllers/QuestionnaireController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WelcareForYou_Iteration_1.Models;
+using System.Data;
+using System.Data.Entity;
+using System.Net;
+
+namespace WelcareForYou_Iteration_1.Controllers
+{
+    public class QuestionnaireController : Controller
+    {
+        private Iteration1ModelContainer db = new Iteration1ModelContainer();
+
+        // GET: Questionnaire
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Result(Int32 salary, Int32 numOfPeople, Int32 currentRent)
+        {
+            var acceptableRent = salary * 0.3;
+            List<Housing> housingList = db.Housings.Where(x => x.MediumPrice <= acceptableRent)
+                                                .Where(x => x.NumOfRoom >= numOfPeople)
+                                                .OrderBy(x => x.MediumPrice).Take(6).ToList();
+            ViewBag.item1diff = housingList[0].MediumPrice/ currentRent;
+            ViewBag.item2diff = housingList[0].MediumPrice / currentRent;
+            ViewBag.item3diff = housingList[0].MediumPrice / currentRent;
+            ViewBag.item4diff = housingList[0].MediumPrice / currentRent;
+            ViewBag.item5diff = housingList[0].MediumPrice / currentRent;
+            ViewBag.item6diff = housingList[0].MediumPrice / currentRent;
+            return View(housingList);
+        }
+    }
+}
