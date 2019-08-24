7915e827081b0df4457b577de64360331b2b73eb
HSINHAN CHUNG <hchu0004@student.monash.edu>
2019-08-24 18:09:37 +1000

    Update Questionnaire Controller

--- WelcareForYou-Iteration-1/Controllers/QuestionnaireController.cs
+++ WelcareForYou-Iteration-1/Controllers/QuestionnaireController.cs
@@ -21,19 +21,38 @@ namespace WelcareForYou_Iteration_1.Controllers
         }
 
         [HttpPost]
-        public ActionResult Result(Int32 salary, Int32 numOfPeople, Int32 currentRent)
+        public ActionResult Result(String salary, String numOfPeople1, String currentRent1)
         {
-            var acceptableRent = salary * 0.3;
+            var numOfPeople = int.Parse(numOfPeople1);
+            var currentRent = int.Parse(currentRent1);
+            var acceptableRent = int.Parse(salary) * 0.3;
+
             List<Housing> housingList = db.Housings.Where(x => x.MediumPrice <= acceptableRent)
                                                 .Where(x => x.NumOfRoom >= numOfPeople)
-                                                .OrderBy(x => x.MediumPrice).Take(6).ToList();
-            ViewBag.item1diff = housingList[0].MediumPrice/ currentRent;
-            ViewBag.item2diff = housingList[0].MediumPrice / currentRent;
-            ViewBag.item3diff = housingList[0].MediumPrice / currentRent;
-            ViewBag.item4diff = housingList[0].MediumPrice / currentRent;
-            ViewBag.item5diff = housingList[0].MediumPrice / currentRent;
-            ViewBag.item6diff = housingList[0].MediumPrice / currentRent;
+                                                .OrderByDescending(x => x.MediumPrice).Take(6).ToList();
+
+            List<int> diffList = new List<int>();
+            var item1diff = housingList[0].MediumPrice * 100 / currentRent - 100;
+            var item2diff = housingList[1].MediumPrice * 100 / currentRent - 100;
+            var item3diff = housingList[2].MediumPrice * 100 / currentRent - 100;
+            var item4diff = housingList[3].MediumPrice * 100 / currentRent - 100;
+            var item5diff = housingList[4].MediumPrice * 100 / currentRent - 100;
+            var item6diff = housingList[5].MediumPrice * 100 / currentRent - 100;                       
+            diffList.Add(item1diff);
+            diffList.Add(item2diff);
+            diffList.Add(item3diff);
+            diffList.Add(item4diff);
+            diffList.Add(item5diff);
+            diffList.Add(item6diff);
+            ViewData["diff"] = diffList;
+
             return View(housingList);
+        }
+
+        protected override void HandleUnknownAction(string actionName)
+        {
+            Response.Redirect("~/Home/Index");
+            base.HandleUnknownAction(actionName);
         }
     }
 }
